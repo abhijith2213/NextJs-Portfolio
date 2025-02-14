e821bafb80e9df740456a112f9912a19b738f7f0
abhijith2213 <abhijithas2213@gmail.com>
2025-02-14 22:19:51 +0400

    vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 import { ThemeProvider } from "./provider"
 
